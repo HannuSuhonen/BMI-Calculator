3c20d9dd4b1477501c17c7aa6a0fc52597f6dd09
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-04-03 00:34:47 +0300

    slider add

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
 }
 
