5f406673873edc96048c524e59f59b14f7b73d46
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-04-04 00:39:53 +0300

    texts update, sliders update

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -8,15 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weightText: UILabel!
+    @IBOutlet weak var heightText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        let roundedValue = round(sender.value * 100)/100
+        heightText.text = "\(String(roundedValue)) M"
     }
     
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value)
+        weightText.text = "\(String(roundedValue)) KG"
+    }
 }
 
