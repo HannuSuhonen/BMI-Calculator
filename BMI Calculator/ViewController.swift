44a7855b3a824ceebd6a2f862fa485e0f22ac93e
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-04-06 00:58:30 +0300

    vmi formula added

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var bmiText: UILabel!
     @IBOutlet weak var weightText: UILabel!
     @IBOutlet weak var heightText: UILabel!
+    var height: Float = 0
+    var weight:Float = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        height = 1.5
+        weight = 100
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         let roundedValue = round(sender.value * 100)/100
         heightText.text = "\(String(roundedValue)) M"
+        height = roundedValue
+        
+        let bmi = calculateBmi(height, weight)
+        
+        bmiText.text = String(bmi)
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         let roundedValue = round(sender.value)
         weightText.text = "\(String(roundedValue)) KG"
+        weight = roundedValue
+        
+        let bmi = calculateBmi(height, weight)
+        
+        bmiText.text = String(bmi)
+    }
+    
+    func calculateBmi(_: Float, _: Float) -> Float{
+        return weight/(height * height)
     }
 }
 
